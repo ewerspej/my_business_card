c87ac353ebf945a80c49c1b0bdcae60113421e16
Julian Ewers <ewerspej@tcd.ie>
2023-05-15 11:38:57 +0200

    added back button and label to QrCodePage

--- MyBusinessCard/QR/QrCodePage.xaml.cs
+++ MyBusinessCard/QR/QrCodePage.xaml.cs
@@ -2,9 +2,14 @@ namespace MyBusinessCard.QR;
 
 public partial class QrCodePage
 {
-	public QrCodePage(QrCodeViewModel viewModel)
-	{
-		InitializeComponent();
+    public QrCodePage(QrCodeViewModel viewModel)
+    {
+        InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
     }
 }
