853d4f8c21900f1db0f2215b557de327eef700f9
Julian Ewers <ewerspej@tcd.ie>
2023-05-15 11:53:33 +0200

    fixed up QrCodePage layout

--- MyBusinessCard/QR/QrCodePage.xaml.cs
+++ MyBusinessCard/QR/QrCodePage.xaml.cs
@@ -2,14 +2,25 @@ namespace MyBusinessCard.QR;
 
 public partial class QrCodePage
 {
+    private readonly QrCodeViewModel _viewModel;
+
     public QrCodePage(QrCodeViewModel viewModel)
     {
         InitializeComponent();
-        BindingContext = viewModel;
+        BindingContext = _viewModel = viewModel;
     }
 
     private async void OnBackButtonClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("..");
+    }
+
+    private async void OnShareButtonClicked(object sender, EventArgs e)
+    {
+        await Share.Default.RequestAsync(new ShareTextRequest
+        {
+            Title = _viewModel.Title,
+            Text = _viewModel.Link
+        });
     }
 }
