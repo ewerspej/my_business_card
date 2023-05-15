68e15ab2392a1e617b684579fa0555817572eddb
Julian Ewers <ewerspej@tcd.ie>
2023-05-15 11:31:42 +0200

    added links for blog and website and improved QrCodePage layout

--- MyBusinessCard/MainPage.xaml.cs
+++ MyBusinessCard/MainPage.xaml.cs
@@ -15,4 +15,16 @@ public partial class MainPage : ContentPage
         var link = HttpUtility.UrlEncode("http://linkedin.com/in/jewerspeters");
         await Shell.Current.GoToAsync(nameof(QrCodePage) + $"?Link={link}&Title=LinkedIn");
     }
+
+    private async void OnBlogClicked(object sender, EventArgs e)
+    {
+        var link = HttpUtility.UrlEncode("http://ewerspej.hashnode.dev");
+        await Shell.Current.GoToAsync(nameof(QrCodePage) + $"?Link={link}&Title=Blog");
+    }
+
+    private async void OnWebButtonClicked(object sender, EventArgs e)
+    {
+        var link = HttpUtility.UrlEncode("http://ewerspej.gatsbyjs.io");
+        await Shell.Current.GoToAsync(nameof(QrCodePage) + $"?Link={link}&Title=Website");
+    }
 }
